5bb4456202b5ae7c837fc05335389bed57cd8bbc
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-08-14 01:46:34 +0600

    connect with mongo

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.13qkung.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('My choice server has been connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.get('/', (req, res) =>{
     res.send('My Choice shop server ruuning');
 })
