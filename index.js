f917798019e788b6f6885c373bbb15c7d4e52a4d
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-09-08 16:23:56 +0600

    first commit after transfer remote origin

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.13qkung.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -30,25 +29,6 @@ async function run(){
             const allProducts = await cursor.toArray();
             res.send(allProducts);
         });
-
-        app.get('/product/:id', async(req, res) =>{
-            const id = req.params.id;            
-            const query = {_id:ObjectId(id)}            
-            const product = await productCollection.findOne(query);              
-            res.send(product);
-
-        });
-
-        //POST
-        app.post('/product', async(req, res) =>{
-            const newProduct = req.body;
-            const result = await productCollection.insertOne(newProduct);
-            res.send(result);
-        });
-
-
-
-         
     }
     finally{
 
